e9cf8e56c1f4fd5ea1a3e7bc67148d6f99402a72
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-02 14:03:02 +0100

    added

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const pool = new importPool({
   user: "postgress",
   host: "localhost",
   database: "students",
-  password: "dennisfortune11",
+  password: keys?.psqlPassword,
   port: 2024,
 });
 
