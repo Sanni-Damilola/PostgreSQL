33c730558e002f814eb8bd6b74a1acaa60ca472d
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-02 14:31:03 +0100

    added

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const studentRoutes = require("./src/students/routes");
 const app = express();
 const port = 2023;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.use("/api/v1/students", studentRoutes)
+app.use("/api/v1/students", studentRoutes);
 
 app.listen(port, () => {
   console.log("app listening on", port);
