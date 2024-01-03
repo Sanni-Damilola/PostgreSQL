71a1b2bcc4111f1ba4ab7fa00c64cb89dd6b4e2f
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-03 11:21:09 +0100

    added

--- src/students/controller.js
+++ src/students/controller.js
@@ -1,7 +1,7 @@
 const data = require("../../config/db");
 
 const getStudents = (req, res) => {
-  data.query("SELECT * FROM students", (error, result) => {
+  data.query("", (error, result) => {
     if (error) throw error;
     res.status(200).json(result.rows);
   });
---
+++ src/students/queries.js
@@ -0,0 +1,1 @@
+const getStudents = "SELECT * FROM students";
